02835b44c4f2f8ee5123b91aad6b512466351634
Jeff Borda <jeffborda@outlook.com>
2019-10-22 21:07:12 -0700

    added Settings and TaskDetails activities, saving username to shared pref's, displaying username on main activity

--- app/src/main/java/com/jeffborda/taskmaster2/activities/MainActivity.java
+++ app/src/main/java/com/jeffborda/taskmaster2/activities/MainActivity.java
@@ -1,12 +1,15 @@
 package com.jeffborda.taskmaster2.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.jeffborda.taskmaster2.R;
 
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View event) {
-                Log.i(TAG, "Add Task button clicked.");
+                Log.i(TAG, "'Add Task' button clicked.");
                 Intent addTaskIntent = new Intent(MainActivity.this, AddTask.class);
                 startActivity(addTaskIntent);
             }
@@ -35,10 +38,33 @@ public class MainActivity extends AppCompatActivity {
         allTasksButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View event) {
-                Log.i(TAG, "All Tasks button clicked.");
+                Log.i(TAG, "'All Tasks' button clicked.");
                 Intent allTasksIntent = new Intent(MainActivity.this, AllTasks.class);
                 startActivity(allTasksIntent);
             }
         });
+
+        Button settingsButton = findViewById(R.id.main_activity_button_settings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View event) {
+                Log.i(TAG, "'Settings' button clicked.");
+                Intent settingsIntent = new Intent(MainActivity.this, Settings.class);
+                startActivity(settingsIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        // Required to override the method
+        super.onResume();
+        // Setup default Shared Preferences
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        // Get username from Shared Preferences
+        String username = sharedPreferences.getString("username", "user");
+
+        TextView mainActivityHeading = findViewById(R.id.heading_main_activity);
+        mainActivityHeading.setText(username + getResources().getString(R.string.heading_with_username_main_activity));
     }
 }
---
+++ app/src/main/java/com/jeffborda/taskmaster2/activities/TaskDetails.java
@@ -0,0 +1,16 @@
+package com.jeffborda.taskmaster2.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jeffborda.taskmaster2.R;
+
+public class TaskDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_details);
+    }
+}
