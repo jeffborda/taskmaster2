2becf287f720d38fbc8eeb40693eaa2f031d5e9a
Jeff Borda <jeffborda@outlook.com>
2019-10-29 10:16:45 -0700

    recycler view rendering Tasks from http request

--- app/src/main/java/com/jeffborda/taskmaster2/activities/MainActivity.java
+++ app/src/main/java/com/jeffborda/taskmaster2/activities/MainActivity.java
@@ -5,20 +5,34 @@ import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Room;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.jeffborda.taskmaster2.R;
 import com.jeffborda.taskmaster2.adapters.TaskItemAdapter;
 import com.jeffborda.taskmaster2.models.Task;
 import com.jeffborda.taskmaster2.models.TaskmasterDatabase;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -34,12 +48,19 @@ public class MainActivity extends AppCompatActivity implements TaskItemAdapter.O
         setContentView(R.layout.activity_main);
 
 
-        this.renderRecyclerViewFromDatabase();
+        // Http Get Request
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://taskmaster-api.herokuapp.com/tasks")
+                .build();
+        okHttpClient.newCall(request).enqueue(new LogHttpDataCallback(this));
+
+        // RecyclerView Setup
+        //TODO: this can be commented back in when rendering Tasks from local database
+//        this.renderRecyclerViewFromDatabase();
 
 
         // Button Setup
-        // Reference the button using its ID
-        // Add event listener to the button
         Button addTaskButton = findViewById(R.id.main_activity_button_add_task);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -119,5 +140,51 @@ public class MainActivity extends AppCompatActivity implements TaskItemAdapter.O
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         // Define Adapter class that is able to communicate with RecyclerView
         recyclerView.setAdapter(new TaskItemAdapter(this.tasks, this));
+    }
+
+    public void renderTasksListFromHttpData(String httpResponseData) {
+        this.tasks = new LinkedList<>();
+        Gson gson = new Gson();
+        Type typeOf = new TypeToken<LinkedList<Task>>(){}.getType();
+        this.tasks = gson.fromJson(httpResponseData, typeOf);
+        RecyclerView recyclerView = findViewById(R.id.task_items_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new TaskItemAdapter(this.tasks, this));
+    }
+
+    class LogHttpDataCallback implements Callback {
+
+        private MainActivity mainActivityInstance;
+        private final static String TAG = "LogHttpDataCallback";
+
+        LogHttpDataCallback(MainActivity mainActivityInstance) {
+            this.mainActivityInstance = mainActivityInstance;
+        }
+
+        @Override
+        public void onFailure(@NotNull Call call, @NotNull IOException e) {
+            Log.e(TAG, "Internet Error");
+            Log.e(TAG, e.getMessage());
+        }
+
+        // onResponse is called by OkHttp if the request is successful.
+        @Override
+        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+            String responseBody = response.body().string();
+            Log.i(TAG, "Http Response: " + responseBody);
+
+            // Now you can call another method in the MainActivity class
+            //mainActivityInstance.renderHttpData(responseBody);
+
+            // Handler allows us to pass info from another thread back to the main thread.
+            Handler handlerForMainThread = new Handler(Looper.getMainLooper()) {
+                @Override
+                public void handleMessage(Message inputMessage) {
+                    mainActivityInstance.renderTasksListFromHttpData((String)inputMessage.obj);
+                }
+            };
+            Message completeMessage = handlerForMainThread.obtainMessage(0, responseBody);
+            completeMessage.sendToTarget();
+        }
     }
 }
